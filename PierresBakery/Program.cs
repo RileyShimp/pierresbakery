d783d03eca6855c3979285170c5f0d7a009aec13
Riley Shimp <rilryshimp@gmail.com>
2022-02-25 14:12:40 -0800

    Add UI to take order and display total

--- PierresBakery/Program.cs
+++ PierresBakery/Program.cs
@@ -16,6 +16,24 @@ namespace PierresBakery
       Console.WriteLine("Our Pastries are $2.00 a each.");
       Console.WriteLine("Or 3 for $5.00!");
       Console.WriteLine("---------------------------");
+      Console.WriteLine("---------------------------");
+      Console.WriteLine("How many loaves of bread would you like?");
+      string stringBreadQuantity = Console.ReadLine();
+      int breadQuantity = int.Parse(stringBreadQuantity);
+      Console.WriteLine("How many pastries would you like?");
+      string stringPastryQuantity = Console.ReadLine();
+      int pastryQuantity = int.Parse(stringPastryQuantity);
+      Bread breadOrder = new Bread(breadQuantity);
+      Pastry pastryOrder = new Pastry(pastryQuantity);
+      int breadTotal = breadOrder.GetPrice();
+      int pastryTotal = pastryOrder.GetPrice();
+      int orderTotal = breadTotal + pastryTotal;
+      Console.WriteLine("---------------------------");
+      Console.WriteLine("YOUR ORDER");
+      Console.WriteLine("Bread: " + breadQuantity);
+      Console.WriteLine("Pastries: " + pastryQuantity);
+      Console.WriteLine("YOUR TOTAL IS: $" + orderTotal + ".00");
+      Console.WriteLine("Thanks for shopping at Pierre's Bakery!");
     }
   }
 }
